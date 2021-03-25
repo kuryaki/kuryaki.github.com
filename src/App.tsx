c7fc0e14198346330079be7a2ef0b79e0628df57
David Roncancio <david@roncancio.me>
2021-03-25 05:19:32 -0400

    feat: convert to react + typescript

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>David Roncancio</h2>
+        <p>
+          <a className="App-link" target="_blank" href="mailto:david@roncancio.me">e-mail</a> |
+          <a className="App-link" target="_blank" href="https://github.com/kuryaki">github</a> |
+          <a className="App-link" target="_blank" href="https://twitter.com/kuryaki">twitter</a>
+        </p>
+        <p>Software Developer</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
